2bf6cd1bdac46b06c7fb1bfb18ee9544b6e5655f
NM Kawcher <46804139+nmkawcher@users.noreply.github.com>
2021-05-07 12:28:34 +0600

    folder and file creating and deleting functionality added

--- app/src/main/java/kawcher/initux/myapplication/View/MainActivity.java
+++ app/src/main/java/kawcher/initux/myapplication/View/MainActivity.java
@@ -1,16 +1,25 @@
 package kawcher.initux.myapplication.View;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -33,17 +42,28 @@ public class MainActivity extends AppCompatActivity {
     private List<Server>ipList;
     private Adapter adapter;
     private SharedPrefManager sharedPrefManager;
+    private Button createFileBtn,createFolderBtn,deleteFileBtn,deleteFolderBtn;
+    private File filePath = new File(Environment.getExternalStorageDirectory() + "/ABC.txt");
+    private String folderDirectory="/sdcard/new_dir";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE
+                , Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+
         init();
         loadData();
+        onClickListener();
 
 
 
     }
+
+
 
     private void loadRVData() {
 
@@ -103,5 +123,63 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         recyclerView=findViewById(R.id.rv);
         sharedPrefManager=SharedPrefManager.getInstance(MainActivity.this);
+        createFileBtn=findViewById(R.id.create_file_button);
+        createFolderBtn=findViewById(R.id.create_folder_button);
+        deleteFileBtn=findViewById(R.id.delete_file_button);
+        deleteFolderBtn=findViewById(R.id.delete_folder_button);
     }
+
+    private void onClickListener() {
+
+        createFileBtn.setOnClickListener(v -> createFile());
+
+        deleteFileBtn.setOnClickListener(v -> {
+            if (filePath.exists()){
+                filePath.delete();
+                Toast.makeText(MainActivity.this, "file deleted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        createFolderBtn.setOnClickListener(v -> {
+            File dir = new File(folderDirectory);
+            try{
+                if(dir.mkdir()) {
+                    Toast.makeText(this, "Directory created", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Directory is not created", Toast.LENGTH_SHORT).show();
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        });
+
+        deleteFolderBtn.setOnClickListener(v ->{
+
+            File del = new File("/sdcard/new_dir");
+            boolean success = del.delete();
+            if (!success) {
+                Toast.makeText(this, "Deletion of directory failed!", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "Deletion of directory successful", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    private void createFile() {
+        try {
+            filePath.createNewFile();
+            FileOutputStream stream = new FileOutputStream(filePath);
+            try {
+                stream.write("text-to-write".getBytes());
+            } finally {
+                stream.close();
+            }
+            Toast.makeText(this, "File Created Successfully!", Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
